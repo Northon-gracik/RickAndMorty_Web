5344b349eb74c4e23ba0c2e503d1aa0f1c63b724
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-12 15:58:20 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import App from './App.jsx'
+import Hello from './rickAndMorty/Hello'
 
 ReactDOM.render(
-    <App/>,
+    <BrowserRouter>
+        <Hello/>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route exact path="/hello" component={Hello}/>
+        </Switch>
+    </BrowserRouter>,
     document.getElementById('root')
 )
---
+++ src/rickAndMorty/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Hello.css'
+
+export default props => {
+    return (
+        <div className="nav">
+            <h1>Hello World !!!</h1>
+        </div>
+    )
+    
+}
