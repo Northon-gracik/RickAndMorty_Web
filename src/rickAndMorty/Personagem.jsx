72d213bc0bc046349bcbe5ea65aa75aceebf94ef
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-14 15:33:11 -0300

    Update Personagem.jsx

--- src/rickAndMorty/Personagem.jsx
+++ src/rickAndMorty/Personagem.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import axios from 'axios'
 
 export default async url => {
-    var personagem = await axios.get(url)
-    window.alert((personagem.data.name))
+    // var personagem = await axios.get(url)
+    // window.alert((personagem.data.name))
     return (
         <div>
-            <p>{JSON.stringify(await personagem.data.name)}</p>
+            {/* <p>{JSON.stringify(await personagem.data.name)}</p> */}
         </div>
     )
 }
