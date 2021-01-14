27fa9364b7b0f273429f179735199c8f136106de
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-14 11:19:33 -0300

    Update EpisodeSingle.jsx

--- src/rickAndMorty/EpisodeSingle.jsx
+++ src/rickAndMorty/EpisodeSingle.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import axios from './data/axios'
+import episodios from './data/episodios'
 
 export default props =>{
+    // props.match.params.id
+    var episodio = episodios.filter(episodio => episodio.id == props.match.params.id)[0]
     return (
         <div>
-            <h1>{props.params.id}</h1>
+            <h1>Episodio: {episodio.name}</h1>
+            <h2>{episodio.air_date}</h2>
+            
         </div>
-    )
-} 
+        )
+    } 
