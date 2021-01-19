7f51af852b170c9d8e4c69fd7c87c8d70ad7e941
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-19 11:20:12 -0300

    Update Localizacoes.jsx

--- src/rickAndMorty/Localizacoes.jsx
+++ src/rickAndMorty/Localizacoes.jsx
@@ -13,7 +13,6 @@ export default props => {const axios = require('axios');
             <div className="s"> 
                 <div >
                     {eps.map(ep => <Localizacao key={ep.id} loc={ep}/>)}
-                    
                 </div>
             </div>
 )
