d37458ff686f89b3681abc6a643dc0a0f2cff588
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-19 11:07:25 -0300

    Update Episodios.jsx

--- src/rickAndMorty/Episodios.jsx
+++ src/rickAndMorty/Episodios.jsx
@@ -4,14 +4,14 @@ import axios from 'axios'
 import './episode.css'
 
 import Episode from './Episode';
-import episodios from './data/episodios'
+import Episodios from './data/episodios'
 
 export default (props) => {
     
     return(
             <div className="s">
                 <div >
-                    {episodios.map(ep => <Episode key={ep.id} eps={ep}/>)}
+                    {Episodios.map(ep => <Episode key={ep.id} eps={ep}/>)}
                 </div>
             </div>
     )
