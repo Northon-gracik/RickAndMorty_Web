15d2b0ce01ab0c60e275ae82549ca0b4d6cb25e1
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-18 22:12:21 -0300

    Update dataEpisodios.js

---
+++ src/rickAndMorty/data/dataEpisodios.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import {useState} from 'react'
+
+export default () => {
+    const [episodios, setEpisodios] = useState([]);
+
+    axios.get('https://rickandmortyapi.com/api/episode/?page=1')
+        .then(resp => setEpisodios(resp.data.results))
+}
