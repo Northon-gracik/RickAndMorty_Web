93894c6b6177f5e51b3da857db1836304962ef04
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-19 11:07:30 -0300

    Update Localizacoes.jsx

--- src/rickAndMorty/Localizacoes.jsx
+++ src/rickAndMorty/Localizacoes.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
 
+import Localizacao from './localizacao'
+
 export default props => {const axios = require('axios');
 
     const [eps, setEps] = useState([]);
-
-
-    (async () => {
-        const teste = await axios.get(`https://rickandmortyapi.com/api/location?page=${1}`)
-        setEps(teste.data.results)
-    })()
+ 
+    axios.get(`https://rickandmortyapi.com/api/location?page=${1}`)
+    .then(resp => setEps(resp.data.results))
+    
+    
 
     return(
             <div className="s"> 
                 <div >
-                    {eps.map(ep => <div>{ep.name}</div>)}
+                    {eps.map(ep => <Localizacao key={ep.id} loc={ep}/>)}
                     
                 </div>
             </div>
