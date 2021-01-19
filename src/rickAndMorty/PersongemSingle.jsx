1d0a6016d99a9b0b26a85da8baf29a7c37e8bf69
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-19 20:35:32 -0300

    Update PersongemSingle.jsx

--- src/rickAndMorty/PersongemSingle.jsx
+++ src/rickAndMorty/PersongemSingle.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default props => {
+    const axios = require('axios');
+
+    const [personagem, setPersonagem] = useState([])
+
+    axios.get(`https://rickandmortyapi.com/api/character/${props.match.params.id}`)
+        .then(resp => setPersonagem(resp.data))
+
+
     return (
-        <div></div>
+        <div>
+            <h1>Personagem: {personagem.name}</h1>
+            <p>{personagem.species} - {personagem.status} - {personagem.gender} {personagem.type}</p>
+            
+        </div>
     )
 }
