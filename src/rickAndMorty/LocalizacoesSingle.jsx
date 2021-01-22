ac70697114c5435e22ad42504970045a77b1de0d
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 16:20:32 -0300

    Update LocalizacoesSingle.jsx

--- src/rickAndMorty/LocalizacoesSingle.jsx
+++ src/rickAndMorty/LocalizacoesSingle.jsx
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from 'react'
+import Person from './person';
+
+import './Hello.css'
+
 
 export default props => {
     const axios = require('axios');
 
     const [loc, setLoc] = useState([])
+    const [residents, setResidents] = useState([])
 
     useEffect(() => {
         axios.get(`https://rickandmortyapi.com/api/location/${props.match.params.id}`)
             .then(resp => setLoc(resp.data))
+
+        axios.get(`https://rickandmortyapi.com/api/location/${props.match.params.id}`)
+            .then(resp => setResidents(resp.data.residents))
     },[])
 
     return (
         <div>
-            <p>{loc.name}</p>
+            <h1>{loc.name}</h1>
+            <div className='album'>
+                {residents.map(resi => <Person url={resi}/>)}
+            </div>
         </div>
     )
 }
