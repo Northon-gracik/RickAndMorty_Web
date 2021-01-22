0a7d66e71510985683f2a6a6d92627ebcc6a1b27
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 15:19:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import Episodios from './rickAndMorty/Episodios';
 import EpisodeSingle from './rickAndMorty/EpisodeSingle';
 import Personagens from './rickAndMorty/personagens';
 import PersonagemSingle from './rickAndMorty/PersongemSingle';
+import LocalizacoesSingle from './rickAndMorty/LocalizacoesSingle';
 
 
 ReactDOM.render(
@@ -22,6 +23,7 @@ ReactDOM.render(
             <Route exact path="/episodios" component={Episodios}/>
             <Route exact path="/episodios/:id" component={EpisodeSingle}/>
             <Route exact path="/localizacoes" component={Localizacoes}/>
+            <Route exact path="/localizacoes/:id" component={LocalizacoesSingle}/> 
             <Route exact path="/personagens" component={Personagens}/>
             <Route exact path="/personagens/:id" component={PersonagemSingle}/>
         </Switch>
