339a37b0758a46db6f130d9a19cf181e14b66cce
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 14:40:01 -0300

    Update personagens.jsx

--- src/rickAndMorty/personagens.jsx
+++ src/rickAndMorty/personagens.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Personagem from './Personagem'
 
@@ -7,8 +7,10 @@ export default props =>{
 
     const [personagem, setPersonagem] = useState([]);
 
-    axios.get('https://rickandmortyapi.com/api/character/?page=1')
-        .then(resp => setPersonagem(resp.data.results))
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character/?page=1')
+            .then(resp => setPersonagem(resp.data.results))
+    },[])
 
     return(
         <div> {personagem.map(person => <Personagem key={person.id} person={person}/>)} </div>
