c492a1e650d85701f4d7f516cef719ccafacab9f
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 15:19:10 -0300

    Update localizacao.jsx

--- src/rickAndMorty/localizacao.jsx
+++ src/rickAndMorty/localizacao.jsx
@@ -5,6 +5,8 @@ export default ({loc}) => {
         <div>
             <p>{loc.name}</p>
             <p>{loc.residents.length}</p>
+            <p>{loc.type}</p>
+            <a href={`localizacoes/${loc.id}`}>Veja Mais</a>
             <br/>
         </div>
     )
