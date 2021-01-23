6821e0d8c8900a540870450ced22c284648105b2
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 23:46:38 -0300

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,11 @@ import './App.css'
 export default props => {
     return (
         <div className="Footer">
-
+            <p>Northon Gracik</p>
+            <a href="https://github.com/Northon-gracik/RickAndMorty_Web" target="_blank">
+                <img src="https://github.com/fluidicon.png" width={40}></img> 
+            </a>
+            
         </div>
     )
 }
