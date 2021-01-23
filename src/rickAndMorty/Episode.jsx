7cddc3dc0590c24098297301efafb16aae846bac
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 21:32:49 -0300

    Update Episode.jsx

--- src/rickAndMorty/Episode.jsx
+++ src/rickAndMorty/Episode.jsx
@@ -4,13 +4,11 @@ import './episode.css'
 
 export default ({eps}) => {
     return(
-        <div className="container">
-            <div className="view">
-                <h2>{eps.name}</h2>
-                <h2>{eps.characters.length} Personagens</h2> 
-                <h2>{eps.episode}</h2>
-                <a href={`/episodios/${eps.id}`}><p>Veja Mais</p></a>
-            </div>
+        <div className="view">
+            <h2>{eps.name}</h2>
+            <h2>{eps.characters.length} Personagens</h2> 
+            <h2>{eps.episode}</h2>
+            <a href={`/episodios/${eps.id}`}><p>Veja Mais</p></a>
         </div>
     )
  }
