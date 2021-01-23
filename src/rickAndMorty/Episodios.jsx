21230fa668124e4cc477257a43481a9cfdcbd939
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-22 23:53:52 -0300

    Update Episodios.jsx

--- src/rickAndMorty/Episodios.jsx
+++ src/rickAndMorty/Episodios.jsx
@@ -8,10 +8,17 @@ export default props => {
     const axios = require('axios');
 
     const [episodios, setEpisodios] = useState([])
+    const [info, setInfo] = useState([])
 
     useEffect(() => {
         axios.get('https://rickandmortyapi.com/api/episode/?page=1')
             .then(resp => setEpisodios(resp.data.results))    
+        
+        axios.get(`https://rickandmortyapi.com/api/episode/?page=1`)
+            .then(resp => {setInfo(resp.data.info)
+                alert(JSON.stringify(info))})
+
+        
     },[])
 
 
