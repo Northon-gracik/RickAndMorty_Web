539cc84f7bba891adb0545c940a8f1d10d989330
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-26 20:21:43 -0300

    Update Localizacoes.jsx

--- src/rickAndMorty/Localizacoes.jsx
+++ src/rickAndMorty/Localizacoes.jsx
@@ -8,17 +8,30 @@ export default props => {
     const axios = require('axios');
 
     const [eps, setEps] = useState([]);
+    const [info, setInfo] = useState([])
+
+    var key = [];
  
     useEffect(() => {
-        axios.get(`https://rickandmortyapi.com/api/location?page=${1}`)
+        axios.get(`https://rickandmortyapi.com/api/location?page=${props.match.params.id}`)
             .then(resp => setEps(resp.data.results))
+
+        axios.get(`https://rickandmortyapi.com/api/location`)
+            .then(resp => resp.data.info.pages)
+            .then(resp => setInfo(resp))
     },[])
     
+    for(var i = 0; i < info; i++){
+        key.push(i + 1)
+    }
     
     return(
-            <div> 
+            <div style={{display: 'flex', flex: 1 , flexDirection: 'column'}}> 
                 <div  className="album">
                     {eps.map(ep => <Localizacao key={ep.id} loc={ep}/>)}
+                </div>
+                <div className="album">
+                    {key.map(key => <a href={`/localizacoes/${key}`} className="space" >{key}</a>)}
                 </div>
             </div>
 )
