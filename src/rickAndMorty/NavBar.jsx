58f6bc6d695eaebd7e6d4300b979d4107cf2bbbf
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-26 20:21:40 -0300

    Update NavBar.jsx

--- src/rickAndMorty/NavBar.jsx
+++ src/rickAndMorty/NavBar.jsx
@@ -8,8 +8,8 @@ export default props => {
             <a className="nav-img" href="/"><img src="https://res.cloudinary.com/bambam/image/upload/v1526900336/logo-2.png" width="150" /></a>
             <div>
                 <a className="nav-link" href="/episodios/1">Episodios</a>
-                <a className="nav-link" href="/localizacoes">localizacoes</a>
-                <a className="nav-link" href="/personagens">personagens</a>
+                <a className="nav-link" href="/localizacoes/1">localizacoes</a>
+                <a className="nav-link" href="/personagens/1">personagens</a>
             </div>
         </div>
     )
